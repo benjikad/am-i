b4a345484747dbff49762d31ff3a90aa16254408
benjikad <123773963+benjikad@users.noreply.github.com>
2025-06-03 14:31:24 -0400

    Update 404.js

--- api/404.js
+++ api/404.js
@@ -1,4 +1,9 @@
+// // api/404.js
+// export default function handler(req, res) {
+//   res.status(404).json({ message: 'Not found' });
+// }
+
 // api/404.js
 export default function handler(req, res) {
-  res.status(404).json({ message: 'Not found' });
+  res.redirect(302, '/html/404.html');
 }
