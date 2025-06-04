bc63cfa72d4f99af9a58c1562177d4fe161e9dd0
benjikad <123773963+benjikad@users.noreply.github.com>
2025-06-04 12:39:24 -0400

    Update hand.js

--- public/hand.js
+++ public/hand.js
@@ -1,1 +1,12 @@
+const fs = require('fs');
+const path = require('path');
 
+export default function handler(req, res) {
+    const filePath = path.join(process.cwd(), 'private/api/main.js');
+    if (fs.existsSync(filePath)) {
+        res.setHeader('Content-Type', 'application/javascript');
+        res.status(200).send(fs.readFileSync(filePath, 'utf8'));
+    } else {
+        res.status(404).send('File not found');
+    }
+}
